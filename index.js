5ef42651c30669e3554c43cc18f087c5288a8d4f
Nikhil Siwan <developer.nikk@gmail.com>
2024-10-26 14:52:16 +0530

    Home Page Done

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/read-blogs",(req,res)=>{
+    res.render("blog.ejs");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("index.ejs");
+});
+
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is running at ${port}`);
+});
