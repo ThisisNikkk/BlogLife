42d0951410a77b28c49ff1064353b33eee00602e
Nikhil Siwan <developer.nikk@gmail.com>
2024-10-28 13:56:07 +0530

    Create blog added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let blogs=[];
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
@@ -19,6 +23,23 @@ app.get("/home",(req,res)=>{
 
 app.post("/create-blogs",(req,res)=>{
     res.render("create.ejs");
+});
+
+app.post("/created-blogs", (req, res) => {
+    const { title, author, category, content } = req.body;
+    const blog = {
+        title: title || "Untitled",
+        author: author || "Anonymous",
+        category: category || "General",
+        content: content || "No content available",
+        image: req.file ? req.file.filename : "default-image.jpg"
+    };
+    blogs.push(blog);
+    res.redirect("/read-blogs");
+});
+
+app.get("/read-blogs", (req, res) => {
+    res.render("user.ejs", { blogs });
 });
 
 
